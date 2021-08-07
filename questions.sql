094c44911d2a6e4d3b59872a20756f34fb4511bd
arryan <arryan@umich.edu>
2021-08-07 17:13:59 -0400

    find most popular registration date

--- questions.sql
+++ questions.sql
@@ -3,3 +3,12 @@
 SELECT * FROM USERS
 ORDER BY created_at LIMIT 0,5;
 
+-- What day of the week do most users register on?
+-- We need to figure out when to schedule an ad campgain
+SELECT
+  DATE_FORMAT(created_at, "%W") AS "day_of_week",
+  COUNT(*) AS "count"
+FROM users
+GROUP BY day_of_week
+ORDER BY count DESC
+LIMIT 2;
