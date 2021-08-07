0137ecff4aa4ba2be0f1c39e81e6692a0c1bf81e
arryan <arryan@umich.edu>
2021-08-07 17:22:02 -0400

    identify inactive users (users with no photos)

--- questions.sql
+++ questions.sql
@@ -12,3 +12,10 @@ FROM users
 GROUP BY day_of_week
 ORDER BY count DESC
 LIMIT 2;
+
+-- We want to target our inactive users with an email campgain
+-- Find the users who have never posted a photo
+SELECT username AS "inactive_users" FROM users
+LEFT JOIN photos
+  ON users.id = photos.user_id
+WHERE photos.user_id IS NULL;
